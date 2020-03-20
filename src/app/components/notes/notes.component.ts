6422733a310b8e87f07bc5eeb4c2fd310923f27a
Tyler Ford <tylerford@gmail.com>
2020-03-19 22:53:26 -0600

    add note constructor

---
+++ src/app/components/notes/notes.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Note } from '../../models/Note';
+
+@Component({
+  selector: 'app-notes',
+  templateUrl: './notes.component.html',
+  styleUrls: ['./notes.component.css']
+})
+export class NotesComponent implements OnInit {
+  notes:Note[];
+  constructor() { }
+
+  ngOnInit() {
+    this.notes = [
+      {
+        id:1,
+        title: 'Build an Angular project',
+        completed: false,
+      },
+      {
+        id:2,
+        title: 'Practice Typescript',
+        completed: false,
+      },
+      {
+        id:3,
+        title: 'Learn Alpine Js',
+        completed: false,
+      },
+      {
+        id:4,
+        title: 'Play with Python',
+        completed: false,
+      },
+    ]
+  }
+
+}
