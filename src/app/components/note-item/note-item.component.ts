b59455d461ecb39e5456710c6d98ca4bdb0d484a
Tyler Ford <tylerford@gmail.com>
2020-03-19 23:48:16 -0600

    add toggle

--- src/app/components/note-item/note-item.component.ts
+++ src/app/components/note-item/note-item.component.ts
@@ -20,4 +20,15 @@ export class NoteItemComponent implements OnInit {
     }
     return classes;
   }
+
+  onToggle(note) {
+    console.log('toggle');
+    note.completed = !note.completed
+  }
+
+  onDelete(note) {
+    console.log('delete');
+    
+  }
+
 }
